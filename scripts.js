76386f05618e1851d947bccba148f46edaed2e0e
CrinaIolanda <68914450+CrinaIolanda@users.noreply.github.com>
2021-02-01 14:22:18 +0200

    new feature: rainbow animation

--- scripts.js
+++ scripts.js
@@ -1,13 +1,29 @@
 const dino = document.getElementById("dino");
+const rainbow = document.getElementById("rainbow");
 
 function jump() {
+    if (dino.classList != "jump") { 
+        
     dino.classList.add("jump");
-
-    setTimeout (function () {
+    
+    setTimeout(function () {
         dino.classList.remove("jump");
-    }, 300)
+    }, 300);}
+
 }
 
+let isAlive = setInterval ( function () {
+    // get current dino Y position
+    let dinoTop = parseInt(window.getComputedStyle(dino).getPropertyValue("top"));
+  
+
+    // get current rainbow X position
+     let rainbowLeft = parseInt(window.getComputedStyle(rainbow).getPropertyValue("left"));
+    
+   // detect collision
+   if ()
+   
+}, 10);
 
 document.addEventListener("keydown", function (event) {
     jump();
